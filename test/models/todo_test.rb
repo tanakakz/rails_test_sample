3d06c076665956016e8ce88273ec95bffc3401cc
tanakakz <tanakakz.feb.23@gmail.com>
2019-11-08 19:18:15 +0900

    multi test-run test

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -4,4 +4,25 @@ class TodoTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "sleep 1" do
+    sleep 3
+    assert true
+  end
+
+  test "sleep 2" do
+    sleep 3
+    assert true
+  end
+
+  test "sleep 3" do
+    sleep 3
+    assert true
+  end
+
+  test "sleep 4" do
+    sleep 3
+    assert true
+  end
+
 end
